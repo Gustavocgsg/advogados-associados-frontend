aacac5bd850b34ce4b562fa809609a6c05ed6bce
Gustavocgsg <gustavocgsg@gmail.com>
2020-07-20 16:10:59 -0300

    colocando o favicon na pagina

--- pages/404.jsx
+++ pages/404.jsx
@@ -6,7 +6,10 @@ export default function erro_404() {
         <section className="bg-coffer">
             <Head>
                 <link href="../../assets/css/style-site.css" rel="stylesheet" type="text/css" />
-
+                <link rel="icon" href="../public/favicon.svg" type="image/svg+xml" sizes="16x16"></link>
+                <link rel="icon" href="../..public/favicon.svg" type="image/svg+xml" sizes="16x16"></link>
+                <link rel="icon" href="../public/favicon.ico" type="image/x-icon"></link>
+                <link rel="icon" href="public/favicon.ico" type="image/x-icon"></link>
                 
                 <title>Index - erro 404</title>
             </Head>
